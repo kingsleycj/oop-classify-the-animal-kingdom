651876a523aefa9591a3601df43df4a7d52f4950
kingsleycj <primekings.kc@gmail.com>
2022-12-18 23:35:02 +0100

    terminology corrections

--- object-oriented.js
+++ object-oriented.js
@@ -1,42 +1,54 @@
-class withBackbone {
-    constructor() {
-        if (this.constructor === withBackbone) {
-          throw new Error("The class 'withBackbone' cannot be called or instantiated");
-        }
+// base class
+class Animal {
+  #animalName;
+  #phylum;
+
+  constructor(animalName, phylum) {
+    if (this.constructor === Animal) {
+      throw new Error("This class cannot be called or instantiated");
     }
+    this.#animalName = animalName;
+    this.#phylum = phylum;
+  }
+}
+class WithBackbone {
+    temperatureCondition(){
     warmBlooded = true;
-}
-
-class withoutBackbone {
-    constructor() {
-        if (this.constructor === withoutBackbone) {
-          throw new Error(" The class 'withoutBackbone' cannot be called or instantiated");
-        }
     }
-    coldBlooded = true;
 }
 
-class AVES extends withBackbone {
+class WithoutBackbone {
+    temperatureCondition(){
+    coldBlooded = true;
+    }
+}
+
+class AVES extends Animal {
     
 }
+
+class Mammal extends Animal {
+
+}
+
+class Arthropoda extends Animal {
+
+}
+
+class Fish extends Animal {
+
+}
+
+class Amphibia extends Animal {
+
+}
+
+class Reptiles extends Animal {
+
+}
 const bird = new AVES();
-class Mammal extends withBackbone {
-
-}
 const cat = new Mammal();
-class Arthropoda extends withoutBackbone {
-
-}
 const butterfly = new Arthropoda();
-class Fish extends withoutBackbone {
-
-}
 const fish = new Fish();
-class Amphibia extends withoutBackbone {
-
-}
 const frog = new Amphibia();
-class Reptiles extends withoutBackbone {
-
-}
 const tortoise = new Reptiles();
