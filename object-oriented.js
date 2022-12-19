8d37e08239a627653e8e5830db502941ec4f8a00
kingsleycj <primekings.kc@gmail.com>
2022-12-19 02:43:46 +0100

    changed "phylum(grammar error)" to "classification"

--- object-oriented.js
+++ object-oriented.js
@@ -1,146 +1,134 @@
-// base class
+// An Abstract class where all other classes will be referenced from
 class Animal {
-  animalName;
-  phylum;
+    animalName;
+    classification;
 
-  constructor(animalName, phylum) {
-    if (this.constructor === Animal) {
-      throw new Error("This class cannot be called or instantiated");
+    // constructor function that will accept the animalName and classification as parameters
+    constructor(animalName, classification) {
+        if (this.constructor === Animal) {
+        throw new Error("This class cannot be called or instantiated");
+        }
+        this.animalName = animalName;
+        this.classification = classification;
     }
-    this.animalName = animalName;
-    this.phylum = phylum;
-  }
+// a method to return the name of the animal
+    getAnimalName() {
+        return this.animalName;
+    }
+// a method to store the name of the animal in a parameter
+    setAnimalName(animalName) {
+        this.animalName = animalName;
+    }
 
-  getAnimalName() {
-    return this.animalName;
-  }
+    // a method  used to call an 
+    getclassification() {
+        this.classification;
+    }
 
-  setAnimalName(animalName) {
-    this.animalName = animalName;
-  }
-  getPhylum() {
-    this.phylum;
-  }
+    warmBlooded() {
+        this.warmBlooded = true;
+    }
 
-  //   setphylum(phylum){
-  //     this.#phylum = phylum;
-  //   }
-  warmBlooded() {
-    this.warmBlooded = true;
-  }
+    coldBlooded() {
+        this.coldBlooded = true;
+    }
 
-  coldBlooded() {
-    this.coldBlooded = true;
-  }
+    listProperties() {
+        throw new Error("This method cannot be implemented");
+    }
 
-  listProperties() {
-    throw new Error("This method cannot be implemented");
-  }
+    withBackbone() {
+        this.withBackbone = true;
+    }
 
-  withBackbone() {
-    this.withBackbone = true;
-  }
-
-  withoutBackbone() {
-    this.withoutBackbone = true;
-  }
+    withoutBackbone() {
+        this.withoutBackbone = true;
+    }
 }
 
-
-// class WithBackbone {
-//     temperatureCondition(){
-//     warmBlooded = true;
-//     }
-// }
-
-// class WithoutBackbone {
-//     temperatureCondition(){
-//     coldBlooded = true;
-//     }
-// }
 
 class Aves extends Animal {
-  constructor(animalName, phylum) {
-    super(animalName, phylum);
-    this.animalName
-    this.phylum
-    this.withBackbone();
-    this.warmBlooded();
-  }
-
-  listProperties() {
-    console.log(this);
-  }
-}
-
-class Mammal extends Animal {
-    constructor(animalName, phylum) {
-        super(animalName, phylum);
-         this.getAnimalName();
-         this.getPhylum();
+    constructor(animalName, classification) {
+        super(animalName, classification);
+        this.animalName;
+        this.classification;
         this.withBackbone();
         this.warmBlooded();
     }
 
-    listProperties(){
+    listProperties() {
+        console.log(this);
+    }
+}
+
+class Mammal extends Animal {
+    constructor(animalName, classification) {
+        super(animalName, classification);
+        this.getAnimalName();
+        this.getclassification();
+        this.withBackbone();
+        this.warmBlooded();
+    }
+
+    listProperties() {
         console.log(this);
     }
 }
 
 class Arthropoda extends Animal {
-  constructor(animalName, phylum) {
-    super(animalName, phylum);
-    this.getAnimalName();
-    this.getPhylum();
-    this.withoutBackbone();
-    this.coldBlooded();
-  }
+    constructor(animalName, classification) {
+        super(animalName, classification);
+        this.getAnimalName();
+        this.getclassification();
+        this.withoutBackbone();
+        this.coldBlooded();
+    }
 
-  listProperties() {
-    console.log(this);
-  }
+    listProperties() {
+        console.log(this);
+    }
 }
 
 class Fish extends Animal {
-  constructor(animalName, phylum) {
-    super(animalName, phylum);
-    this.getAnimalName();
-    this.getPhylum();
-    this.withBackbone();
-    this.coldBlooded();
-  }
+    constructor(animalName, classification) {
+        super(animalName, classification);
+        this.getAnimalName();
+        this.getclassification();
+        this.withBackbone();
+        this.coldBlooded();
+    }
 
-  listProperties() {
-    console.log(this);
-  }
+    listProperties() {
+        console.log(this);
+    }
 }
 
 class Amphibia extends Animal {
-  constructor(animalName, phylum) {
-    super(animalName, phylum);
-    this.getAnimalName();
-    this.getPhylum();
-    this.withBackbone();
-    this.coldBlooded();
-  }
+    constructor(animalName, classification) {
+        super(animalName, classification);
+        this.getAnimalName();
+        this.getclassification();
+        this.withBackbone();
+        this.coldBlooded();
+    }
 
-  listProperties() {
-    console.log(this);
-  }
+    listProperties() {
+        console.log(this);
+    }
 }
 
 class Reptiles extends Animal {
-  constructor(animalName, phylum) {
-    super(animalName, phylum);
-     this.getAnimalName();
-     this.getPhylum();
-    this.withBackbone();
-    this.coldBlooded();
-  }
+    constructor(animalName, classification) {
+        super(animalName, classification);
+        this.getAnimalName();
+        this.getclassification();
+        this.withBackbone();
+        this.coldBlooded();
+    }
 
-  listProperties() {
-    console.log(this);
-  }
+    listProperties() {
+        console.log(this);
+    }
 }
 
 const aves = new Aves("Bird", "Aves");
@@ -150,29 +138,35 @@ const fish = new Fish("Tilapia", "Fish");
 const amphibia = new Amphibia("Toad", "Amphibia");
 const reptiles = new Reptiles("Turtle", "Reptiles");
 
-aves.setAnimalName("Bird",);
+aves.setAnimalName("Bird");
 mammal.setAnimalName("Cat");
+arthropoda.setAnimalName("Butterfly");
 fish.setAnimalName("Tilapia");
 amphibia.setAnimalName("Toad");
 reptiles.setAnimalName("Turtle");
 
 console.log("\nCLASSIFICATION OF THE ANIMAL KINGDOM\n");
 
-console.log(aves.getAnimalName())
+console.log(aves.getAnimalName());
 aves.listProperties();
+console.log('\n')
 
 console.log(mammal.getAnimalName());
 mammal.listProperties();
+console.log("\n");
 
-arthropoda.setAnimalName();
 console.log(arthropoda.getAnimalName());
 arthropoda.listProperties();
+console.log("\n");
 
 console.log(fish.getAnimalName());
 fish.listProperties();
+console.log("\n");
 
 console.log(amphibia.getAnimalName());
 amphibia.listProperties();
+console.log("\n");
 
 console.log(reptiles.getAnimalName());
 reptiles.listProperties();
+console.log("\n");
